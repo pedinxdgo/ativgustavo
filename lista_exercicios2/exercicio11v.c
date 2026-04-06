5847a3dcc9925618ab624c9e3977a7c8c3047e47
pedinxdgo <pedinxdgo@hotmail.com>
2026-04-06 12:15:33 -0300

    Exercicios

--- lista_exercicios2/exercicio11v.c
+++ lista_exercicios2/exercicio11v.c
@@ -4,9 +4,9 @@
 void main(){
     char palavra[50];
     printf("Digite uma palavra: ");
-    scanf("%s", palavra);
+    scanf("%s", &palavra);
     int i;
-    for(i = strlen(palavra) ; i >= 0; i--) {
-            printf("%c", palavra[i]);
-        }
+    for(i = 50 ; i >= 0; i--) {
+        printf("%c", palavra[i]);
+    }
     }
--- lista_exercicios2/exercicio20.c
+++ lista_exercicios2/exercicio20.c
@@ -4,14 +4,18 @@
 void main(){
     char usuario[50];
     char senha[20];
-    printf("Digite seu usuario: \n");
-    scanf("%s", usuario);
-    printf("Digite sua senha: \n");
-    scanf("%s", senha);
-    int tamanho = strlen(senha);
-    for(int i = tamanho; tamanho<8; i++){
-        printf("A senha precisa ter 8 caracteres\n");
+    int login = 1;
+    while(login == 1){
+        printf("Digite seu usuario: \n");
+        scanf("%s", &usuario);
         printf("Digite sua senha: \n");
-        scanf("%s", senha);
-    }
+        scanf("%s", &senha);
+        if (strcmp(usuario, "admin") == 0 && strcmp(senha, "1234") == 0){
+            printf("Login realizado com sucesso\n");
+            login = 0;
+        }
+        else{
+            printf("Dados de login invalidos\n");
+        }
+}
 }
