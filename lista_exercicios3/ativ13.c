ef27e07e1a6d1f3bd57d32befc12e4685e6920fc
pedinxdgo <pedinxdgo@hotmail.com>
2026-04-27 12:04:44 -0300

    atividade 13

--- lista_exercicios3/ativ13.c
+++ lista_exercicios3/ativ13.c
@@ -3,16 +3,23 @@
 
 void main(){
     int questoes[30];
-    int resposta[30];
-    int gabarito[31] = {'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', '\0'};
+    char resposta[30];
+    char gabarito[31] = {'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'd', '\0'};
     int corretas = 0;
     int incorretas = 0;
-    for(int i = 0; i < 30; i++){
+    for(int i = 0; i < 31; i++){
         printf("Insira a resposta da questão %d", i);
-        scanf("%d", &resposta[i]);
-        if(resposta[i] == gabarito){
+        scanf("%c", &resposta[i]);
+        
+        if(resposta[i] == gabarito[i]){
             corretas++;
         }
+        else{
+            incorretas++;
+        }
     }
+    int porcentagemAcerto = (questoes[30] * corretas) / 100;
+    printf("O aluno acertou %d questoes", corretas);
+    printf("Porcentagem de acerto: %d", porcentagemAcerto);
 
 }
